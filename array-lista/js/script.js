044c1dd6786763b6ae7d6f189ba34972b45e46e8
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-22 18:26:05 +0100

    added variables

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,6 +1,20 @@
  console.log('JS OK')
 
  // CREO L'ARRAY
-
-const frigorifero = ['banana,','mela','pera','ciliegia','arancia','mandarino','cocomero','limone','fragola']
+const frigorifero = ['banana','mela','pera','ciliegia','arancia','mandarino','cocomero','limone','fragola']
+// AGGIUNGO LA PESCA
 frigorifero.push('pesca')
+// RECUPERO L'ELEMENTO
+const paragraphElement= document.getElementById('paragraph')
+let message = '';
+//CREO UN CICLO
+for(let i = 0 ; i < frigorifero.length; i++){
+    if(frigorifero[i] += 'cocomero'){
+        message = "Trovato! Devo solo preparare il cocktail."
+    }else{
+        message = "Oh no, devo uscire a comprare il cocomero!"
+    }
+    paragraphElement.innerText = message
+    
+}
+
