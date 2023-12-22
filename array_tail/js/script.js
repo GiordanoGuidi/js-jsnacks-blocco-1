cb8e9a96266c5cd5425c510cbc23953d67408883
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-22 19:33:32 +0100

    added cicle for

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,1 +1,10 @@
 console.log('JS OK')
+//CREO L'ARRAY
+const arrayNumbers =[] ;
+
+const numOfElements = parseInt(prompt('Quanti elementi deve contenere l\'array?', 7))
+const randomNumbers =  Math.floor((Math.random() * 100) + 1);
+console.log(numOfElements)
+
+for( let i = 0 ; i < numOfElements; i++){
+}
