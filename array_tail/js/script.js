0c84d9b937071a0b6df7d11842670fff7792432b
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-25 11:44:49 +0100

    printed last five numbers of the array

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,10 +1,22 @@
 console.log('JS OK')
 //CREO L'ARRAY
 const arrayNumbers =[] ;
-
+console.log(arrayNumbers)
+// CHIEDO QUANTI ELEMENTI DEVE CONTENERE L'ARRAY
 const numOfElements = parseInt(prompt('Quanti elementi deve contenere l\'array?', 7))
+// GENERO UN NUMERO CASUALE
 const randomNumbers =  Math.floor((Math.random() * 100) + 1);
 console.log(numOfElements)
-
-for( let i = 0 ; i < numOfElements; i++){
+// ? VALIDAZIONE
+if(numOfElements <= 6 || isNaN(numOfElements)){
+    alert('L\'array deve essere un numero maggiore o uguale a 6');
+}
+// CREO CICLO FOR
+for( let i = 1 ; i <= numOfElements; i++){
+    // MOLTIPLICO NUMERO DI ELEMENTI PER IL NUMERO CASUALE
+    const numbers = randomNumbers * (i);
+    // PUSHO I NUMERI RANDOM NELL'ARRAY
+    arrayNumbers.push(numbers);
+    // STAMPO GLI ULTIMI 5 NUMERI DELL'ARRAY
+    console.log(arrayNumbers.slice(-5))
 }
