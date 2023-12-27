151a511185948b66a8b47d14381d0cca7a4e7200
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-27 17:49:22 +0100

    moved randomNumbers inside for

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -4,19 +4,26 @@ const arrayNumbers =[] ;
 console.log(arrayNumbers)
 // CHIEDO QUANTI ELEMENTI DEVE CONTENERE L'ARRAY
 const numOfElements = parseInt(prompt('Quanti elementi deve contenere l\'array?', 7))
-// GENERO UN NUMERO CASUALE
-const randomNumbers =  Math.floor((Math.random() * 100) + 1);
-console.log(numOfElements)
-// ? VALIDAZIONE
+console.log('elementi che costituiscono l\'array', numOfElements)
+// CREO L'ELEMENTO HTML
+const paragraphElement = document.createElement('p')
+console.log('elemento html', paragraphElement)
+
+// # VALIDAZIONE
 if(numOfElements <= 6 || isNaN(numOfElements)){
     alert('L\'array deve essere un numero maggiore o uguale a 6');
 }
 // CREO CICLO FOR
 for( let i = 1 ; i <= numOfElements; i++){
+    // GENERO UN NUMERO CASUALE ALL'INTERNO DEL CICLO
+    const randomNumbers =  Math.floor((Math.random() * 100) + 1);
+    console.log('numero casuale', randomNumbers)
     // MOLTIPLICO NUMERO DI ELEMENTI PER IL NUMERO CASUALE
     const numbers = randomNumbers * (i);
+    console.log('numeri casuali * i', numbers)
     // PUSHO I NUMERI RANDOM NELL'ARRAY
     arrayNumbers.push(numbers);
-    // STAMPO GLI ULTIMI 5 NUMERI DELL'ARRAY
-    console.log(arrayNumbers.slice(-5))
+    console.log('array pieno',arrayNumbers)
 }
+// STAMPO GLI ULTIMI 5 NUMERI DELL'ARRAY
+console.log('array tagliato', arrayNumbers.slice(-5))
