ac06d477a01d4858a1012f4d2ff84dd55bb79164
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-03 11:55:50 +0100

    added cicle for

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,13 +1,12 @@
-console.log('JS OK')
-
 //CREO ARRAY PIù CORTO
 const animals = ['cane','gatto','topo','giraffa'];
-console.log(animals, animals.length)
 // CREO ARRAY PIù LUNGO
 const friends = ['jordi','gabriele','francesco','gianluca','matteo','jacopo','riccardo'];
-console.log(friends, friends.length)
 // CREO JNA CONDIZIONE
 if(friends.length > animals.length){
-    for(let i = 0 ; i < friends.length ; i++){
+    for(let i = 0 ; i < friends.length && friends.length !== animals.length; i++){
+        animals.push(friends[i]);
     }
 }
+console.log(animals);
+console.log(friends)
