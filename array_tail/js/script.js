b76f541bb21093389c9edc527a3c3d7dec6946b4
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-03 11:21:04 +0100

    multiplication modified with [i]

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,12 +1,9 @@
 //CREO L'ARRAY
 const arrayNumbers =[] ;
-console.log(arrayNumbers)
 // CHIEDO QUANTI ELEMENTI DEVE CONTENERE L'ARRAY
 const numOfElements = parseInt(prompt('Quanti elementi deve contenere l\'array?', 7))
-console.log('elementi che costituiscono l\'array', numOfElements)
 // CREO L'ELEMENTO HTML
 const paragraphElement = document.createElement('p')
-console.log('elemento html', paragraphElement)
 // LO COLLEGO AL FILE HTML
 document.body.appendChild(paragraphElement)
 
@@ -18,13 +15,10 @@ if(numOfElements < 6 || isNaN(numOfElements)){
 for( let i = 1 ; i <= numOfElements; i++){
     // GENERO UN NUMERO CASUALE ALL'INTERNO DEL CICLO
     const randomNumbers =  Math.floor((Math.random() * 100) + 1);
-    console.log('numero casuale', randomNumbers)
     // MOLTIPLICO NUMERO DI ELEMENTI PER IL NUMERO CASUALE
-    const numbers = randomNumbers * (i);
-    console.log('numeri casuali * i', numbers)
+    const numbers = randomNumbers * [i];
     // PUSHO I NUMERI RANDOM NELL'ARRAY
     arrayNumbers.push(numbers);
-    console.log('array pieno',arrayNumbers)
 }
 // STAMPO GLI ULTIMI 5 NUMERI DELL'ARRAY IN PAGINA
 paragraphElement.innerText = (arrayNumbers.slice(-5))
